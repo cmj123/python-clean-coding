22171d4ff71b8ac34b5bdfca085c2cf61f680094
Esuabom Dijemeni <e.dijemeni@gmail.com>
2022-09-08 14:42:36 +0100

    Completed PEP8 Imports and Dunder Names

--- section3_pep8_code_layout/imports.py
+++ section3_pep8_code_layout/imports.py
@@ -4,6 +4,22 @@
 # Import third party libraries second
 # Import local packages third
 # Imports should always be at the top of the file
+# Description of file comes before import
+# After file description import future updates... 
+# Future update should always appear after the file description
+# dunder variables should be assigned after future imports
+
+
+'''
+    This file is a professional example to PEP8 imports
+'''
+
+from __future__ import unicode_literals
+
+__all__ = ['a', 'b', 'c', 'd']
+__version__ = '0.1'
+__author__ = "Esuabom Dijemeni"
+
 
 import os
 import sys
@@ -13,3 +29,12 @@ import flask
 from django import get_version
 
 from spacing_encoding import Person, alpha
+
+def a():
+    pass
+
+def b():
+    pass
+
+def c():
+    pass
